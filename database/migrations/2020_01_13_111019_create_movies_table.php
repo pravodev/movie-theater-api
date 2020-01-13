2650fd63f0121c629a3dacf485bb2f41899b8373
Rifqi Khoeruman Azam <pravodev@gmail.com>
2020-01-13 21:09:06 +0800

    create model and migration

---
+++ database/migrations/2020_01_13_111019_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->unsignedInteger('rating')->nullable();
+            $table->date('release_date');
+            $table->unsignedInteger('duration')->comment('duration in minutes');
+            $table->string('director');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
