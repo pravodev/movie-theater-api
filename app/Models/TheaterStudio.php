05760fd5a9a43b63b06b9c99882058f9e8a91329
Rifqi Khoeruman Azam <pravodev@gmail.com>
2020-01-15 01:15:59 +0800

    define fillable attribute

--- app/Models/TheaterStudio.php
+++ app/Models/TheaterStudio.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class TheaterStudio extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'total_seat',
+        'theater_id'
+    ];
+
+    public function theater()
+    {
+        return $this->belongsTo(Theater::class);
+    }
 }
