17baa4bfb08d2c3d7cb53dae820d2ab5c98342da
Rifqi Khoeruman Azam <pravodev@gmail.com>
2020-01-14 21:40:04 +0800

    create resources for all endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('movies', 'MovieController');
+Route::apiResource('genres', 'GenreController');
+Route::apiResource('casts', 'CastController');
+Route::apiResource('theaters', 'TheaterController');
