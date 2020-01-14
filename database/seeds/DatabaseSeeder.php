31e989cb333528f2d7654387503be0fc52c16321
Rifqi Khoeruman Azam <pravodev@gmail.com>
2020-01-14 20:14:22 +0800

    theater seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(MovieSeeder::class);
+        $this->call([
+            MovieSeeder::class,
+            TheaterSeeder::class
+        ]);
     }
 }
--- database/seeds/TheaterSeeder.php
+++ database/seeds/TheaterSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Theater;
 use Faker\Factory;
 use Carbon\Carbon;
+use App\Models\TheaterStudio;
 
 class TheaterSeeder extends Seeder
 {
@@ -14,20 +15,44 @@ class TheaterSeeder extends Seeder
      */
     public function run()
     {
-        $theaters = ['Anggrek 21 Cinema', 'Arion 21 Cinema', 'Binjai 21 Cinema', 'Citra 21 Cinema', 'Gading XXI Cinema', 'Grand 21 Cinema', 'International 21 Cinema'];
-        
         DB::beginTransaction();
 
         $faker = Factory::create();
 
         try {
-            foreach($theaters as $theater){
-                
+            $totalData = 10;
+            $provinces = [
+                ['West Java', ['Bandung', 'Jakarta']],
+                ['East Java', ['Semarang']],
+                ['East Kalimantan', ['Balikpapan']]
+            ];
+
+            for ($i=0; $i < $totalData; $i++) { 
+                shuffle($provinces);
+                $province = $provinces[0];
+
+                $attribute = [
+                    'name' => $faker->text('20') . ' Cinema',
+                    'about' => $faker->paragraph,
+                    'address' => $faker->address,
+                    'city' => $faker->randomElement($province[1]),
+                    'province' => $province[0],
+                    'website' => $faker->url
+                ];
+
+                $theater = new Theater;
+                $theater->fill($attribute)->save();
+                $this->command->info($theater->name . ' created');
+                $totalStudio = $faker->numberBetween(3, 5);
+                for ($j=0; $j < $totalStudio; $j++) { 
+                    $studio = new TheaterStudio;
+                    $studio->name = 'Studio ' . $faker->text('5');
+                    $studio->total_seat = $faker->numberBetween(20, 90);
+                    $studio->theater_id = $theater->id;
+                    $studio->save();
+                }
+
             }
-            // $totalData = 10;
-            // for ($i=0; $i < $totalData; $i++) { 
-            //     # code...
-            // }
             DB::commit();
         } catch (\Throwable $th) {
             DB::rollback();
