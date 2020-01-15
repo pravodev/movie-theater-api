25ef1014abb174745a66060c826467ef613175d8
Rifqi Khoeruman Azam <pravodev@gmail.com>
2020-01-15 22:08:17 +0800

    add show

--- app/Http/Controllers/ShowTimeController.php
+++ app/Http/Controllers/ShowTimeController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Show;
-use App\Http\Requests\ShowResource;
+use App\Http\Resources\ShowResource;
 
 class ShowTimeController extends ApiController
 {
     protected $model = Show::class;
-    protected $resources = ShowResource::class;
+    protected $resource = ShowResource::class;
 }
--- app/Models/Show.php
+++ app/Models/Show.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Show extends Model
 {
-    //
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class ,'movie_id');
+    }
+
+    public function getData()
+    {
+        $request = request();
+        $model = new Show;
+        if($date = $request->query('date')){
+            if($date == 'now'){
+                $date = now();
+            }else{
+                $date = Carbon::parse($date);
+            }
+            
+            $model = $model->whereDate('date', $date);
+        }
+        return $model;
+    }
 }
