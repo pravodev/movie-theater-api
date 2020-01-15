d6ba4052a60f559cebfac4044499f633fc212603
Rifqi Khoeruman Azam <pravodev@gmail.com>
2020-01-15 22:08:34 +0800

    add show

--- app/Http/Resources/ShowResource.php
+++ app/Http/Resources/ShowResource.php
@@ -14,6 +14,11 @@ class ShowResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $movie = $this->movie;
+
+        $data =  parent::toArray($request);
+        $data['movie_title'] = $movie->title;
+        $data['movie'] = new MovieResource($movie);
+        return $data;
     }
 }
