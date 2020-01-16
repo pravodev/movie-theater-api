e01666c2d666100f60928fa83de1a8aae29e4ccd
Rifqi Khoeruman Azam <pravodev@gmail.com>
2020-01-16 19:45:25 +0800

    sesuaikan seed

--- database/seeds/ShowSeeder.php
+++ database/seeds/ShowSeeder.php
@@ -24,23 +24,29 @@ class ShowSeeder extends Seeder
             $totalData = 100;
 
             for ($i=0; $i < $totalData; $i++) { 
-                $movie = $movies->random();
-                $show = new Show;
-                $show->movie_id = $movie->id;
-                $show->date = now()->addDay($faker->numberBetween(0, $totalData));
-                $show->save();
-
-                $theater = $theaters->random();
-                
-                foreach($theater->studios as $studio){
-                    $show_time = new ShowTime;
-                    $show_time->show_id = $show->id;
-                    $show_time->studio_id = $studio->id;
-                    $show_time->theater_id = $theater->id;
-                    $show_time->start_time = $show->date->copy()->addHour($faker->numberBetween(1,24));
-                    $show_time->end_time = $show_time->start_time->copy()->addMinute($movie->duration);
-                    $show_time->total_price = $faker->randomElement([20000, 50000, 90000]);
-                    $show_time->save();
+                $movies = $movies->shuffle();
+                $date = now()->addDay($i);
+                for ($k=0; $k < $faker->numberBetween(4,8); $k++) { 
+                    $movie = $movies[$k];
+                    $show = new Show;
+                    $show->movie_id = $movie->id;
+                    $show->date = $date;
+                    $show->save();
+    
+                    $theaters = $theaters->shuffle();
+                    for ($j=0; $j < $faker->numberBetween(2,4); $j++) { 
+                        $theater = $theaters[$j];
+                        foreach($theater->studios as $studio){
+                            $show_time = new ShowTime;
+                            $show_time->show_id = $show->id;
+                            $show_time->studio_id = $studio->id;
+                            $show_time->theater_id = $theater->id;
+                            $show_time->start_time = $show->date->copy()->addHour($faker->numberBetween(1,24));
+                            $show_time->end_time = $show_time->start_time->copy()->addMinute($movie->duration);
+                            $show_time->total_price = $faker->randomElement([20000, 50000, 90000]);
+                            $show_time->save();
+                        }
+                    }
                 }
 
                 $this->command->info($movie->name. ' scheduled show for date ' . $show->date);
