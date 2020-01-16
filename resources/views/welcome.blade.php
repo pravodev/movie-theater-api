4241ee61c6939f448cea3a92055892f76000af88
Rifqi Khoeruman Azam <azam@comtelindo.com>
2020-01-16 19:31:49 +0800

    show show times

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,17 +91,66 @@
             </div>
             <br>
         </div>
-        <div class="container">
+        <div class="modal fade" id="modalShowTime" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel"><div id="movieTitle"></div></h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                    <h4>2020-01-19</h4>
+
+                    <div class="row">
+                        <div class="col-md-6" id="detailShowTime">
+                            
+                        </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <script src="https://use.fontawesome.com/bfecf810bf.js"></script>
+        <script src='https://cdn.jsdelivr.net/lodash/4.17.2/lodash.min.js'></script>
         <script>
             jQuery(document).ready(function(){
                 var nowPlayingEl = $('#movieNowPlaying')
 
+                function renderShowTimes({data}){
+                    let {show_times} = data
+                    let groupByTheater  = _.groupBy(show_times, (data) => data.theater_id)
+                    console.log("TCL: renderShowTimes -> groupByTheater", groupByTheater)
+                    const detailElement = $('#detailShowTime')
+                    const html = _.map(groupByTheater, ((theater) => {
+                        const timesEl = theater.map(times => (`
+                            <button class="btn btn-sm btn-warning">${times.start_time}</button>
+                        `))
+                        return `
+                            <div>
+                                <h5>${theater[0].theater_name}</h5>
+                                ${timesEl.join('')}
+                            </div>
+                        `
+                    })).join('')
+                    
+                    detailElement.html(html)
+                    console.log("TCL: renderShowTimes -> groupByTheater", groupByTheater)
+                    console.log("TCL: renderShowTimes -> show_times", show_times)
+
+                }
+
                 function renderToNowPlaying(shows){
-                    let cards = shows.map(show => (`
+                    let cards = shows.map((show, index) => (`
                         <div class="card" style="width: 17rem;">
                             <img src="{{asset('/dummy-image-portrait.jpg')}}" height="200" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -110,13 +159,31 @@
                                 <b>Direktor</b>
                                 ${show.movie.synopsis}
                                 </p>
-                                <a href="/show-times/${show.id}" class="btn btn-primary">See Showtimes</a>
+                                <button data-toggle="modal" data-target="#modalShowTime" class="btn btn-primary showModal" data-index="${index}">See Showtimes</button>
                             </div>
                         </div>
                     `))
                     
                     nowPlayingEl.html(cards.join(''))
+
+                    $('.showModal').on('click', function(){
+                        var index = $(this).data('index')
+                        console.log("TCL: renderToNowPlaying -> index", index)
+                        var data = shows[index]
+                        console.log("TCL: renderToNowPlaying -> data", data)
+                        $('#movieTitle').html(data.movie_title)
+
+                        fetch(`/api/shows/${data.id}?include=show_times`)
+                            .then(response => response.json())
+                            .then(data => renderShowTimes(data))
+                            .catch(error => {
+                                console.log('error showTimes', error)
+                            })
+                        // window.alert('asdasd');
+                        
+                    })
                 }
+                
                 
                 fetch('/api/shows?date=now')
                     .then(response => response.json())
